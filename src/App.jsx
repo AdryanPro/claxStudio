06e32e498000abc0abe3bdf478424f0c70d1faeb
AdryanPro <adryan-pro@hotmail.com>
2025-03-23 16:52:04 +0100

    done with the WorksNavPage starting the component that will be the top of every project page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import "./App.css"
 import WorksNavPage from './components/WorksNavPage'
+import TopProject from './components/TopProject'
 function App() {
 
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/WorksNav' element={<WorksNavPage />}/>
+        <Route path='/renderComponent' element={<TopProject />}/>
         {/* <Route path='*' element={<Error />}/> */}
       </Routes>
     </>
--- src/components/WorksNavPage.jsx
+++ src/components/WorksNavPage.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
-
+import "../CSS/worksPage.css"
+import { Link } from 'react-router-dom'
 export default function WorksNavPage() {
   return (
-    <div>
+    <div className='worksPageWrapper'>
         <ul>
-            <l1><span>(01)</span> MOOLY:</l1>
-            <p>Création de marque.</p>
+            <Link href="">
+            <li><span>(01)</span> MOOLY :</li>
+            <p>Création de marque.</p></Link>
 
-            <li><span>(02)</span>MISCIBLE:</li>
-            <p>Design écoresponsable et luxe.</p>
+            <Link href="">
+            <li><span>(02)</span>MISCIBLE :</li>
+            <p>Design écoresponsable et luxe.</p></Link>
 
+            <Link href="">
             <li><span>(03)</span>Ethereal Design :</li>
-            <p>Innovation et architecture.</p>
+            <p>Innovation et architecture.</p></Link>
 
+            <Link href="">
             <li><span>(04)</span>PLaisirs éphémères :</li>
-            <p>Set design et photographie.</p>
+            <p>Set design et photographie.</p></Link>
 
+            <Link href="">
             <li><span>(05)</span>Magazine Studio 54 :</li>
-            <p>Design éditorial.</p>
+            <p>Design éditorial.</p></Link>
 
+            <Link href="">
             <li><span>(06)</span>Italians Do It Better : </li>
-            <p>Affiche illustrative et direction artistique.</p>
+            <p>Affiche illustrative et direction artistique.</p></Link>
 
+            <Link href="">
             <li><span>(07)</span>NUIT ÉTOILÉE :</li>
-            <p>Illustration et adaptation à un public cible.</p>
+            <p>Illustration et adaptation à un public cible.</p></Link>
 
+            <Link href="">
             <li><span>(08)</span>KILOUTOU :</li>
-            <p>Campagne 360°</p>
+            <p>Campagne 360°</p></Link>
         </ul>
     </div>
   )
