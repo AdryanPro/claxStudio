6931edebbd0602ca441f830e56866fab982ae4cc
AdryanPro <adryan-pro@hotmail.com>
2025-06-28 11:06:05 +0200

    works Paage over just will need to change the links of the new project

--- src/components/WorksNavPage.jsx
+++ src/components/WorksNavPage.jsx
@@ -3,61 +3,79 @@ import "../CSS/worksPage.css"
 import "../CSS/worksPageMobile.css"
 import { Link } from 'react-router-dom'
 import BigC from "../assets/menu/menuC.png"
+import BigCMobile from "../assets/menu/BigCWorksMenuMobile.png"
 
 export default function WorksNavPage() {
   return (
     <div className='worksPageWrapper'>
       <div className="workLeftWrapper">
-      <h1 className='h1MenuWorkMobile'>WORKS</h1>
-      <h1 className='h1MenuWorkCoolFontMobile'>Works</h1>
+                <img src={BigCMobile} alt="Logo" className='LogoWorksMobile' />
+        <h1 className='h1MenuWorkMobile'>WORKS</h1>
+        <h1 className='h1MenuWorkCoolFontMobile'>Works</h1>
+        {/* Supprime a partir dici */}
         <ul>
-          <li>
+          <li className='casseCouilleLesP'>
             <Link to='/Molly' onClick={() => window.scrollTo(0, 0)}>
-              <p className='titleWorksMenu'><span>(01)</span> MOLLY :</p>
-              <p className='subTxt'>Création de marque.</p>
+              <p className='titleWorksMenu'><span>(01)</span>MOLLY :</p>
+              <p className=''><span className='invisibleSpan'>(01)</span>Création de marque.</p>
             </Link>
           </li>
 
           <li>
             <Link to="/Miscible" onClick={() => window.scrollTo(0, 0)}>
               <p className='titleWorksMenu'><span>(02)</span>MISCIBLE :</p>
-              <p className='subTxt'>Design écoresponsable et luxe.</p></Link>
+              <p className=''><span className='invisibleSpan'>(02)</span>Design écoresponsable et luxe.</p></Link>
           </li>
 
           <li>
             <Link to="/EtherealDesigne" onClick={() => window.scrollTo(0, 0)}>
                         <p className='titleWorksMenu'><span>(03)</span>Ethereal Design :</p>
-                        <p className='subTxt'>Innovation et architecture.</p></Link>
+                        <p className=''><span className='invisibleSpan'>(03)</span>Innovation et architecture.</p></Link>
           </li>
 
           <li>
             <Link to="/PlaisirÉphémères" onClick={() => window.scrollTo(0, 0)}>
                         <p className='titleWorksMenu'><span>(04)</span>PLaisirs éphémères :</p>
-                        <p className='subTxt'>Set design et photographie.</p></Link>
+                        <p className=''><span className='invisibleSpan'>(04)</span>Set design et photographie.</p></Link>
           </li>
 
           <li>
           <Link to="/MagazineStudio54" onClick={() => window.scrollTo(0, 0)}>
             <p className='titleWorksMenu'><span>(05)</span>Magazine Studio 54 :</p>
-            <p className='subTxt'>Design éditorial.</p></Link>
+            <p className=''><span className='invisibleSpan'>(05)</span>Design éditorial.</p></Link>
           </li>
 
           <li>
           <Link to="/ItaliansDoItBetter" onClick={() => window.scrollTo(0, 0)}>
             <p className='titleWorksMenu'><span>(06)</span>Italians Do It Better : </p>
-            <p className='subTxt'>Affiche illustrative et direction artistique.</p></Link>
+            <p className=''><span className='invisibleSpan'>(06)</span>Affiche illustrative et direction artistique.</p></Link>
           </li>
 
           <li>
           <Link to="/NuitÉtoilée" onClick={() => window.scrollTo(0, 0)}>
                       <p className='titleWorksMenu'><span>(07)</span>NUIT ÉTOILÉE :</p>
-                      <p className='subTxt'>Illustration et adaptation à un public cible.</p></Link>
+                      <p className=''><span className='invisibleSpan'>(07)</span>Illustration et adaptation à un public cible.</p></Link>
           </li>
 
           <li>
           <Link to="/Kiloutou" onClick={() => window.scrollTo(0, 0)}>
                       <p className='titleWorksMenu'><span>(08)</span>KILOUTOU :</p>
-                      <p className='subTxt'>Campagne 360°</p></Link>
+                      <p className=''><span className='invisibleSpan'>(08)</span>Campagne 360°</p></Link>
+          </li>
+          <li>
+          <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                      <p className='titleWorksMenu'><span>(09)</span>LOUÉ :</p>
+                      <p className=''><span className='invisibleSpan'>(09)</span>DESIGN PACKAGING</p></Link>
+          </li>
+          <li>
+          <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                      <p className='titleWorksMenu'><span>(10)</span>HOUSE OF CARDS  :</p>
+                      <p className=''><span className='invisibleSpan'>(10)</span>CRÉATION DE GÉNÉRIQUE</p></Link>
+          </li>
+          <li>
+          <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                      <p className='titleWorksMenu'><span>(11)</span>PLUM LIVING :</p>
+                      <p className=''><span className='invisibleSpan'>(11)</span>MOTION DESIGN</p></Link>
           </li>
         </ul>
       </div>
